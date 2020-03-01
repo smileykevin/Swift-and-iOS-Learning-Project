20b73efbec9ec6f7ce39a97ea5a9b7a8633219bf
Smiley Kevin <smiley@SmileydeMacBook-Air.local>
2020-03-01 10:53:08 +0800

    init project

---
+++ Swift & iOS Learning Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift & iOS Learning Project
+//
+//  Created by Smiley on 2020/3/1.
+//  Copyright © 2020 Smiley Kevin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
